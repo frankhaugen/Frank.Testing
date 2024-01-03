93e4e64adefd7a8b9eb9a4039a651bdd94a82827
Frank R. Haugen <frank.haugen@gmail.com>
2024-01-04 00:32:08 +0100

    Update LoggingBuilderExtensions for test output
    A reference to Xunit.Abstractions has been added in LoggingBuilderExtensions.cs. The method AddPulseFlowTestLoggingProvider has been adjusted to include an ITestOutputHelper and a LogLevel parameter. This enables the redirection of log output to help facilitate testing and debugging.

--- Frank.Testing.Logging/LoggingBuilderExtensions.cs
+++ Frank.Testing.Logging/LoggingBuilderExtensions.cs
@@ -4,6 +4,8 @@ using Frank.PulseFlow.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using Xunit.Abstractions;
+
 namespace Frank.Testing.Logging;
 
 public static class LoggingBuilderExtensions
@@ -15,9 +17,10 @@ public static class LoggingBuilderExtensions
     /// <param name="outputHelper">The ITestOutputHelper to redirect the logging output to.</param>
     /// <param name="logLevel">The log level to use for the test logging. Default is LogLevel.Debug.</param>
     /// <returns>The modified ILoggingBuilder with the test logging added.</returns>
-    public static ILoggingBuilder AddPulseFlowTestLoggingProvider(this ILoggingBuilder builder)
+    public static ILoggingBuilder AddPulseFlowTestLoggingProvider(this ILoggingBuilder builder, ITestOutputHelper outputHelper, LogLevel logLevel = LogLevel.Debug)
     {
         builder.AddPulseFlow();
+        builder.Services.AddSingleton(outputHelper);
         builder.Services.AddSingleton<IFlow, TestLoggingOutputFlow>();
         return builder;
     }
