5bda8ab84384a9f3de0f0c94d06e339d838c9794
Frank R. Haugen <frank.haugen@gmail.com>
2024-01-31 11:18:53 +0100

    Add service replacement method and service tests
    A new method has been added to the ServiceCollectionExtensions that allows service replacement in the IServiceCollection. A new class, WebHostApplicationTestBaseWithStartupTests, has also been added to handle service-specific unit tests. This addition facilitates

--- Frank.Testing.TestBases/WebHostExtensions.cs
+++ Frank.Testing.TestBases/WebHostExtensions.cs
@@ -1,11 +1,31 @@
 ﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Frank.Testing.TestBases;
 
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection Replace<TService, TImplementation>(this IServiceCollection services)
+        where TService : class
+        where TImplementation : class, TService
+    {
+        var descriptors = services.Where(d => d.ServiceType == typeof(TService)).ToList();
+        if (!descriptors.Any())
+            return services;// throw new InvalidOperationException($"No service of type {typeof(TService)} has been registered.");
+
+        foreach (var descriptor in descriptors)
+        {
+            services.Remove(descriptor);
+            var clone = new ServiceDescriptor(typeof(TService), typeof(TImplementation), descriptor.Lifetime);
+            services.Add(clone);
+        }
+
+        return services;
+    }
+}
+    
 public static class WebHostExtensions
 {
     public static HttpClient CreateTestClient(this IWebHost host)
---
+++ Frank.Testing.Tests/TestBases/WebHostApplicationTestBaseWithStartupTests.cs
@@ -0,0 +1,97 @@
+﻿using System.Net;
+
+using FluentAssertions;
+
+using Frank.Testing.TestBases;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+
+using Xunit.Abstractions;
+
+namespace Frank.Testing.Tests.TestBases;
+
+public class WebHostApplicationTestBaseWithStartupTests(ITestOutputHelper outputHelper) : WebHostApplicationTestBase(outputHelper)
+{
+    protected override Task SetupAsync(IWebHostBuilder builder)
+    {
+        builder.UseStartup<Startup>();
+        builder.ConfigureServices(services =>
+        {
+            services.Replace<IService, CoolService>();
+        });
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public async Task Test()
+    {
+        var service = Services.GetRequiredService<IService>();
+        
+        service.DoSomething();
+    }
+}
+
+public interface IService
+{
+    void DoSomething();
+}
+
+public class MyService : IService
+{
+    private readonly ILogger<MyService> _logger;
+
+    public MyService(ILogger<MyService> logger)
+    {
+        _logger = logger;
+    }
+
+    public void DoSomething()
+    {
+        _logger.LogInformation("DoSomething");
+    }
+}
+
+public class CoolService : IService
+{
+    private readonly ILogger<CoolService> _logger;
+
+    public CoolService(ILogger<CoolService> logger)
+    {
+        _logger = logger;
+    }
+
+    public void DoSomething()
+    {
+        _logger.LogInformation("DoSomething else");
+    }
+}
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddSingleton<IService, MyService>();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            // endpoints.MapHealthChecks("/health");
+            endpoints.MapControllers();
+            endpoints.MapGet("/test2", async httpContext =>
+            {
+                await httpContext.Response.WriteAsync("Test2 endpoint");
+            });
+            
+        });
+    }
+}
